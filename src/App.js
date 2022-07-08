00e5ed3d056d74ac2b00f6f634d87c5fb91cbe29
unknown <gabrielayoku@gmail.com>
2022-07-08 14:28:39 +0100

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
-
+import sun from './Components/images/icon-sun.svg'
+import moon from './Components/images/icon-moon.svg'
+import suna from './Components/images/bg-desktop-light.jpg'
+import muna from './Components/images/bg-desktop-dark.jpg'
+import bsuna from './Components/images/bg-mobile-light.jpg'
+import bmuna from './Components/images/bg-mobile-dark.jpg'
+//import plus from './Components/images/icon-cross.svg'
+import cross from './Components/images/p1.png'
+import cross2 from './Components/images/p.png'
+import { useEffect, useRef, useState } from 'react';
+import Body from './Components/images/Body';
 function App() {
+  const[back,setBack]=useState(moon)
+  const[head,setHead]=useState(suna)
+  const[bod,setBod]=useState('rgb(247, 245, 240)')
+  const[color,setColor]=useState('white')
+  const[items,setItems]=useState(0)
+  const [ray,setRay]=useState('rgb(102, 100, 96)')
+  const[arrex,setArrex]=useState([])
+  const[crossimage,setCross]=useState(cross2)
+  const[changing,setChange]=useState('')
+  const[catt,setCat]=useState('black')
+  const[textcolor,setText]=useState()
+  //console.log(arrex.length)
+  useEffect(()=>{
+    if(arrex.length !== 0){
+    localStorage.setItem('magic',JSON.stringify(arrex))
+    }
+    else if (arrex.length === 0){
+      setArrex(JSON.parse(localStorage.getItem('magic')))
+    }
+    else{
+      localStorage.setItem('magic',JSON.stringify(arrex))
+    }
+  },[arrex])
+  //localStorage.setItem('magic',JSON.stringify(arrex))
+  //console.log(arrex.length)
+  //localStorage.clear('magic')
+  function click(){
+    if(back === moon){
+      if(window.screen.width >420){
+        setBack(sun)
+        setHead(`url(${muna})`)
+        setBod('black')
+        setColor('rgb(41,40,38)')
+        setRay('rgb(124, 117, 128)')
+        setCross(cross)
+        setCat('white')
+        setText('#cacde8')
+      }
+      else{
+        setBack(sun)
+        setHead(`url(${bmuna})`)
+        setBod('black')
+        setColor('rgb(41, 40, 38)')
+        setRay('124, 117, 128')
+        setCross(cross)
+        setCat('white')
+        setText('#cacde8')
+      }
+    }
+    else if( back === sun){
+      if(window.screen.width > 420){
+        setBack(moon)
+        setHead(`url(${suna})`)
+        setBod('rgb(247, 245, 240)')
+        setColor('white')
+        setRay('rgb(102, 100, 96)')
+        setCross(cross2)
+        setCat('black')
+        setText('black')
+      }
+      else {
+        setBack(moon)
+        setHead(`url(${bsuna})`)
+        setBod('rgb(247, 245, 240)')
+        setColor('white')
+        setRay('rgb(102, 100, 96)')
+        setCross(cross2)
+        setCat('black')
+        setText('black')
+      }
+    }
+  }
+  function change(e){
+    setChange(e.target.value)
+  }
+
+  function king(e){
+    if(changing.length !== 0){
+      setArrex([...arrex,changing].reverse())
+      setChange('')
+      refer.current.value= ''
+    }
+  }
+  const refer= useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div  className="App">
+      <header style={{backgroundImage:head}}>
+        <h1>TODO</h1>
+        <img onClick={click} src={back} alt='' ></img>
+        <img  onClick={king} className='zin'src={crossimage} alt =''></img>
+        <input ref={refer} onChange={change} type='text' style={{backgroundColor:color, color:textcolor}} placeholder='Create A new Todo...'></input>
+        
       </header>
+      <section style={{backgroundColor:bod}}>
+        <Body text={textcolor} arrayy={setArrex}  value={items} array={arrex} background={color} colorx={ray} cat={catt}/>
+      </section>
     </div>
   );
 }
---
+++ src/Components/images/Body.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useRef, useState } from 'react'
+import check from './icon-check.svg'
+import cross from './icon-cross.svg'
+const Body = (props) => {
+     const[peep,setPip]=useState()
+    const[order,setOrder]=useState('All')
+    const arr =props.array
+    //console.log(arr.length )
+    const refer= useRef()//using this to access the css property of the li element
+    
+    if( arr.length !== 0){
+        var val =arr.map((hue,index)=>
+        <li onLoad={loads} ref={refer} draggable='true' onDragStart={drag} onDrop={drop} onDragOver={allowDrop} key={index} id={index}>
+            <img alt='' onClick={mark} src={check} id='checker'></img>
+            <span /*on={loads}*/ className='tuna' style={{color:props.text}} >{hue}</span>
+        
+            <img alt='' onClick={deletee} data-action={hue} id='img' src={cross}></img>
+        </li>)
+    }
+    
+    
+    //console.log(refer.current)
+    function loads(e){
+        const targets = e.target.children
+       /*const  tuna = Array.from(targets.children).filter(function (sandwich) {
+            return sandwich.matches('#checker');
+        })*/
+        //console.log(targets)
+        if(targets ==='line-through'){
+
+        }
+    }
+    
+    function mark(e){
+        const deed= e.target
+        const cane = deed.style.background
+        const leaf = deed.nextElementSibling
+        if(cane ==='linear-gradient(135deg, rgb(87, 221, 255), rgb(192, 88, 243))'){
+            deed.style.background= 'inherit'
+            deed.style.border='1px solid grey'
+            leaf.style.textDecoration='none'
+            leaf.style.opacity='100%'
+        }
+        else{
+            deed.style.background= 'linear-gradient(135deg,#57ddff,#c058f3'
+            deed.style.background='1px solid inherit'
+            leaf.style.textDecoration='line-through'
+            leaf.style.opacity='50%'
+        }
+
+    }
+    function deletee(e){
+        const eve = e.target.dataset.action
+         const everi = arr.findIndex(even)
+         function even(val){
+          return  val === eve
+         }
+         arr.splice(everi,1)
+         props.arrayy([...arr])
+    }
+    function allowDrop(ev) {
+        ev.preventDefault();
+      }
+      
+      function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+      }
+      
+      function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+      }
+    function flag(e){
+        const key =e.target
+        const action =key.dataset.action
+        if( action === 'all'){
+            setPip('game')
+        }
+        else if (action === 'active'){
+            setPip('take')
+        }else if (action === 'completed'){
+            setPip('tikin')
+        }
+    }
+    function clear(){
+
+    }
+    function mouse(e){
+        e.target.style.color=props.cat
+    }
+    function out(e){
+        e.target.style.color=props.colorx
+    }
+    if (order === 'All'){
+        return (
+            <div className='image' style={{backgroundColor:props.background}}>
+                <main>
+                    <ul >
+                        {val}
+                    </ul>
+                </main>
+                <footer >
+                    <p  style={{color:props.colorx}}>{props.value} item(s) left</p>
+                    <ul  onClick={flag}>
+                        <li  onMouseOver={mouse} onMouseOut={out} style={{color:props.colorx}} id='data' data-action='all'>All</li>
+                        <li onMouseOver={mouse} onMouseOut={out} style={{color:props.colorx}}  id='data' data-action='active'>Active</li>
+                        <li onMouseOver={mouse} onMouseOut={out} style={{color:props.colorx}} id='data' data-action='completed'>Completed</li>
+                    </ul>
+                    <p  onMouseOver={mouse} onMouseOut={out} style={{color:props.colorx}} id='come' onClick={clear}>Clear Completed</p>
+                </footer>
+                <span ref={refer}>Drag And Drop To Reorder List</span>
+                
+            </div>
+        )
+    }
+}
+
+export default Body
